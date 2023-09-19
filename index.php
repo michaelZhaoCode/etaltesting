25deaf14b566f4e29c9db811c0280770e9078bd7
Michael Zhao <82855036+michaelZhaoCode@users.noreply.github.com>
2023-09-19 17:54:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -114,6 +114,11 @@ o<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com 
           // Create connection
           $conn = new mysqli($servername, $username, $password, $dbname);
 
+         // Check connection
+          if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
           $conn->close();
 
           echo "hello";
